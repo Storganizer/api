620e6ff9141cb4a312d6b481c85aa5211dac1ec2
Claudio <info@gitcd.io>
2023-08-24 21:37:51 +0200

    Simple api test using flask

---
+++ api/app/test.py
@@ -0,0 +1,7 @@
+class Test(object):
+
+	def __init__(self):
+		print("Hello Friend")
+
+	def getValues(self):
+		return ['hello', 'friend']
---
+++ api/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from app.test import Test
+
+import sys
+import flask
+import json
+
+
+app = flask.Flask('storganizer-api')
+
+def asJson(data):
+  response = flask.make_response(
+    flask.jsonify(data),
+    401,
+  )
+  response.headers["Content-Type"] = "application/json"
+  return response
+
+
+
+# api routes
+@app.route("/test", methods = ['GET'])
+def test():
+  test = Test()
+  return asJson(test.getValues())
+
+
+
+
+# main loop
+if __name__ == "__main__":
+  try:
+    app.run(host="0.0.0.0", debug=True)
+  except Exception as e:
+    pprint(e)
+    sys.exit(1)
