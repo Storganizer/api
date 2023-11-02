c622b980ffefcf80ac7807af382fb512a9e85f92
Claudio <info@gitcd.io>
2023-11-02 21:05:38 +0100

    Messy tryouts, seems v3 is not really ready yet

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,7 @@
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
+import '@material/web/divider/divider.js';
+import '@material/web/menu/menu.js';
+import '@material/web/menu/sub-menu.js';
+import '@material/web/menu/menu-item.js';
