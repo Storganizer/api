712f4be89794789bf875bb55e27907dab7ebea7c
Claudio <info@gitcd.io>
2023-11-03 06:11:56 +0100

    RiotJS it is

---
+++ ui/readme.md
@@ -0,0 +1,21 @@
+# storanizer-ui
+
+
+
+## Start
+
+```
+npm start
+```
+
+## Test
+
+```
+npm test
+```
+
+## Build
+
+```
+npm run build
+```
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,10 @@
-import '@material/web/button/filled-button.js';
-import '@material/web/button/outlined-button.js';
-import '@material/web/checkbox/checkbox.js';
-import '@material/web/divider/divider.js';
-import '@material/web/menu/menu.js';
-import '@material/web/menu/sub-menu.js';
-import '@material/web/menu/menu-item.js';
+import './style.css'
+import '@riotjs/hot-reload'
+import { mount } from 'riot'
+import registerGlobalComponents from './register-global-components'
+
+// register
+registerGlobalComponents()
+
+// mount all the global components found in this page
+mount('[data-riot-component]')
