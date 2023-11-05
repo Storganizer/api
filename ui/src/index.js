a62a23be4e81d2fa44932abfc6fac51412f54a76
Claudio <info@gitcd.io>
2023-11-05 15:54:01 +0100

    DataStore Plugin, no reload and laptop ip

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,10 +1,16 @@
 import './index.css'
 import '@riotjs/hot-reload'
-import { mount } from 'riot'
+import { mount, install } from 'riot'
 import registerGlobalComponents from './register-global-components'
+import dataStore from './dataStore'
 
 // register
 registerGlobalComponents()
 
 // mount all the global components found in this page
 mount('[data-riot-component]')
+
+install(function(component) {
+  // all components will pass through here
+  component.dataStore = dataStore
+})
