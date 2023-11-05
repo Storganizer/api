4976c56a2bb5b5e8b6eb5cf564dbbade5b93cc05
Claudio <info@gitcd.io>
2023-11-05 19:45:49 +0100

    no page reloads

--- ui/src/dataStore.js
+++ ui/src/dataStore.js
@@ -3,28 +3,28 @@ export default {
   locations: false,
 
   getLocations() {
-    if (!this.locations) {
+    /*if (!this.locations) {
       this.fetchLocations()
     }
 
-    return this.locations
+    return this.locations*/
   },
 
   async fetchLocations() {
-    try {
-      const res = await fetch(`http://10.1.1.179:5000/locations`);
+    /*try {
+      const res = await fetch(`http://10.1.1.79:5000/locations`);
       this.locations = await res.json();
     } catch (error) {
       console.log({ error });
-    }
+    }*/
   },
 
   loadLocations() {
-    this.locations = [{
+/*    this.locations = [{
       name: 'test',
       description: 'super',
       classification: 1
     }]
-    return true
+    return true*/
   }
 }
