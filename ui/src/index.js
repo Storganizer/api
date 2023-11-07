6a7c13d4c93c1db8d138cd50870407bb50cf414e
Claudio <info@gitcd.io>
2023-11-07 18:46:13 +0100

    Empty page now

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,34 @@
 import './index.css'
 import '@riotjs/hot-reload'
+
+
+//import * as riot from 'riot'
+
 import * as riot from 'riot'
-import registerGlobalComponents from './register-global-components'
+import App from './components/application.riot'
+
+const mountApp = riot.component(App)
+
+const app = mountApp(
+  document.getElementById('root')
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import registerGlobalComponents from './register-global-components'
 import dataStore from './dataStore'
 
 window.dataStore = dataStore
@@ -10,9 +37,9 @@ registerGlobalComponents()
 
 // mount all the global components found in this page
 riot.mount('[data-riot-component]')
-
-riot.install(function(component) {
+*/
+/*riot.install(function(component) {
   // all components will pass through here
   component.dataStore = dataStore
   return component
-})
+})*/
