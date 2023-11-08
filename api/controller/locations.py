065f0990d99cfb66e9513f26da52a3451315af47
Claudio <info@gitcd.io>
2023-11-08 22:31:14 +0100

    Fully managable locations now - image still missing

--- api/controller/locations.py
+++ api/controller/locations.py
@@ -3,145 +3,127 @@ from model.connection import engine, session
 
 from sqlalchemy import select, text
 from flask_restful import Resource, marshal_with
-
+from flask import request
+import json
+from pprint import pprint
 
 
 class Locations(Resource):
-    """
-    Location definition to use in this classes
-    ---
-    responses:
-      200:
-        description: Schema
-        schema:
-          id: Location
-          properties:
-            name:
-              type: string
-            description:
-              type: string
-            image:
-              type: string
-            classification:
-              type: int
-            boxes:
-              type: list
-    """
-
     def get(self):
-        """
-        Fetch all locations
-        ---
-        tags:
-          - locations
-
-        responses:
-          200:
-            description: All storage locations
-            schema:
-              id: Locations
-              properties:
-                list:
-                  type: list
-                  schema:
-                    $ref: '#/definitions/Location'
-        """
         locations = []
         for location in session.scalars(select(ModelLocation)):
           locations.append(location.getDataTransferObject())
 
 
-        return locations
+        return locations, 200 # OK
 
     def post(self):
-        """
-        Add a new location
-        ---
-        tags:
-          - locations
-        parameters:
-          - in: body
-            name: body
-            schema:
-              $ref: '#/definitions/Location'
-        responses:
-          201:
-            description: The Location has been created
-            schema:
-              $ref: '#/definitions/Location'
-        """
-        return [""]
+        try:
+          location = json.loads(request.data)
 
+          # Fail if name not set or name Empty
+          if not 'name' in location.keys() or location['name'] == '':
+            return {
+              'error': True,
+              'message': 'Name might not be empty'
+            }, 400 # Bad Request
 
+          if 'id' in location.keys() and location['id'] > 0:
+            return {
+              'error': True,
+              'message': f'You cannot update location.id {location["id"]} by post, use put instead'
+            }, 405 # Method not Allowed
 
+          # {'classification': 1, 'description': 'Schlafzimmer', 'id': 0, 'name': 'Lorina'}
+          locationDescription = location['description'] if 'description' in location.keys() else ''
+          locationEntry = ModelLocation(
+            name=location['name'],
+            description=locationDescription,
+            classification=location['classification']
+          )
+          session.add(locationEntry)
+          session.commit()
 
+          return {
+            'error': False,
+            'message': 'Location successfully stored'
+          }, 201 # Created
+        except Exception as e:
+          return {
+            'error': True,
+            'message': f'Exception: {str(e)}'
+          }, 400 # Bad Request
 
-# Todo
-# shows a single todo item and lets you delete a todo item
 class Location(Resource):
 
     def get(self, id):
-        """
-        Get single location
-        ---
-        tags:
-          - locations
-        parameters:
-          - in: path
-            name: id
-            required: true
-            description: Location ID
-            type: string
-        responses:
-          200:
-            description: The location data transfer object
-            schema:
-              $ref: '#/definitions/Location'
-        """
         location = session.query(ModelLocation).get(id)
         if location:
-          return location.getDataTransferObject(["boxes"])
+          return location.getDataTransferObject(["boxes"]), 200 # OK
 
         return ["Location not found"]
 
     def delete(self, id):
-        """
-        Delete a location
-        ---
-        tags:
-          - locations
-        parameters:
-          - in: path
-            name: id
-            required: true
-            description: The Location ID to delete
-            type: string
-        responses:
-          204:
-            description: Location deleted
-        """
-        return '', 204
+        location = session.query(ModelLocation).get(id)
+        if location:
+          session.delete(location)
+          session.commit()
+          return {
+            'error': False,
+            'message': 'Location successfully deleted'
+          }, 202 # accepted
+
+        return {
+          'error': True,
+          'message': f'Location {id} not found'
+        }, 404 # not found
 
     def put(self, id):
-        """
-        Update a location
-        ---
-        tags:
-          - locations
-        parameters:
-          - in: body
-            name: body
-            schema:
-              $ref: '#/definitions/Location'
-          - in: path
-            name: id
-            required: true
-            description: The location ID to be updated
-            type: string
-        responses:
-          201:
-            description: Updated location data
-            schema:
-              $ref: '#/definitions/Location'
-        """
-        return [""]
+        try:
+          location = json.loads(request.data)
+
+          if not 'id' in location.keys():
+            return {
+              'error': True,
+              'message': 'location.id not sent, not updating'
+            }, 400 # Bad Request
+
+          id = int(id)
+          location['id'] = int(location['id'])
+
+          # Fail if id is shady
+          if id <= 0 or id != location['id']:
+            return {
+              'error': True,
+              'message': 'location.id mismatch, not updating'
+            }, 400 # Bad Request
+
+          # Fail if name not set or name Empty
+          if not 'name' in location.keys() or location['name'] == '':
+            return {
+              'error': True,
+              'message': 'Name might not be empty'
+            }, 400 # Bad Request
+
+          if 'id' in location.keys() and location['id'] == 0:
+            return {
+              'error': True,
+              'message': f'You cannot add location by put, use post instead'
+            }, 405 # Method not Allowed
+
+          # {'classification': 1, 'description': 'Schlafzimmer', 'id': 0, 'name': 'Lorina'}
+          locationEntry = session.query(ModelLocation).get(id)
+          locationEntry.name = location['name']
+          locationEntry.description = location['description']
+          locationEntry.classification = location['classification']
+          session.commit()
+
+          return {
+            'error': False,
+            'message': 'Location successfully stored'
+          }, 201 # Created
+        except Exception as e:
+          return {
+            'error': True,
+            'message': f'Exception: {str(e)}'
+          }, 400 # Bad Request
