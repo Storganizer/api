152292eb86a2dba46d2c23ed751ebfaa756663c4
Claudio <info@gitcd.io>
2023-11-08 02:16:01 +0100

    This is even better

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,13 +2,20 @@ import * as riot from 'riot'
 import Application from './components/application/application.riot'
 import DataStore from './components/application/dataStore'
 
+const registry = {
+  dataStore: DataStore
+}
+riot.install(function(component) {
+  // all components will pass through here
+  component.registry = registry
+  return component
+})
+
+
 window.dataStore = DataStore
 
 const mountApp = riot.component(Application)
-
 const app = mountApp(
   document.getElementById('root'),
   {}
 )
-
-window.application = app
