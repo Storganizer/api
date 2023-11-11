a399cfbb97ab4500049b8d62489caaaf77221092
Claudio <info@gitcd.io>
2023-11-11 18:28:12 +0100

    Everything so far

--- ui/cordova-prepare.sh
+++ ui/cordova-prepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 rm -rf ./www
-mkdir ./www
+mkdir -p ./www
 cp ./dist/* ./www
 cp ./public/* ./www
+
+export ANDROID_HOME=/home/claudio/Android/Sdk
+#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-11.0.18.0.10-1.fc37.x86_64
