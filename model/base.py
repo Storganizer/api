08bd99c517ff650779af3702c39a8aa330dcd0fc
Claudio <info@gitcd.io>
2023-11-21 00:12:40 +0100

    Default image for everything

--- model/base.py
+++ model/base.py
@@ -65,6 +65,6 @@ class Base(DeclarativeBase):
         dictionary = dto.__dict__
         if dictionary['image']:
             dictionary['image'] = f'http://10.1.1.79:5000{dictionary["image"]}'
-
-
+        else:
+            dictionary['image'] = f'http://10.1.1.79:5000/static/images/_default-{self.__tablename__}.jpg'
         return dto.__dict__
