8cc5ad3270927d04e3ffb2d268f831256891c58b
Claudio <info@gitcd.io>
2024-02-24 01:14:36 +0100

    Postgres connection

--- model/connection.py
+++ model/connection.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 
 
-engine = create_engine("sqlite+pysqlite:///storganizer.db", echo=True)
+#engine = create_engine("sqlite+pysqlite:///storganizer.db", echo=True)
+engine = create_engine('postgresql+psycopg2://storganizer@localhost/storganizer', echo=True)
 
 session_factory = sessionmaker(bind=engine)
 session = scoped_session(session_factory)
