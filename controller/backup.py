68c5f57deda318f27f9eb3cc9eccb4c53678b2b8
Claudio <info@gitcd.io>
2024-02-25 19:48:21 +0100

    Send the right headers, good enough for now

--- controller/backup.py
+++ controller/backup.py
@@ -6,7 +6,7 @@ from model.connection import engine, session
 import os
 from sqlalchemy import select
 from flask_restful import Resource
-from flask import request
+from flask import request, make_response
 import json
 import base64
 import time
@@ -16,7 +16,6 @@ from pprint import pprint
 class Backup(Resource):
     
     def get(self):
-
       locations = []
       for location in session.scalars(select(ModelLocation).order_by(ModelLocation.name)):
         locations.append(location.getDataTransferObject())
@@ -29,11 +28,20 @@ class Backup(Resource):
       for item in session.scalars(select(ModelItem).order_by(ModelItem.name)):
         items.append(item.getDataTransferObject())
 
-      return {
+      allData = {
         "locations": locations,
         "boxes": boxes,
         "items": items
       }
+
+      response = make_response(json.dumps(allData))
+      response.headers['Content-Type'] = 'application/force-download'
+      response.headers['Content-Disposition'] = 'attachment; filename=backup.json'
+      response.status_code = 200
+
+      return response
+
+
 
 class Restore(Resource):
 
