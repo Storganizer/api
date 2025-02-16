469f61a431112ebb27f0382f126d26e2bfd6674b
Claudio <info@gitcd.io>
2025-02-16 10:15:03 +0100

    dev-start.sh with current working directory

--- dev-start.sh
+++ dev-start.sh
@@ -4,14 +4,12 @@ podman rm -f storganizer-api
 podman rm -f storganizer-postgres
 podman pod rm storganizer
 
-
-
-podman pod create --name storganizer -p 5000:5000
+podman pod create --name storganizer -p 5000:5000 -p 5432:5432
 
 podman  run -it -d\
   --pod=storganizer \
   -e SQLALCHEMY_CONNECTION_STRING='postgresql+psycopg2://storganizer@localhost/storganizer' \
-  -v /var/home/claudio/Development/storganizer/api:/app:z \
+  -v $(pwd)/:/app:z \
   --name storganizer-api storganizer-api:dev
 
 podman run -it -d\
@@ -20,5 +18,5 @@ podman run -it -d\
   -e POSTGRES_DB=storganizer \
   -e POSTGRES_USER=storganizer \
   -e POSTGRES_PASSWORD=storganizer \
-  -v /var/home/claudio/podman/storganizer-api/postgres/:/var/lib/postgresql/data:z \
+  -v $(pwd)/postgres/:/var/lib/postgresql/data:z \
   docker.io/postgres:16-alpine
